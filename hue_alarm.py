cae2c855f73085e379cb9e9a4ae0dec8abf387be
walter5138_charon <piranha@disroot.org>
2020-02-07 20:39:10 +0100

    Added new file hue_alarm.

---
+++ hue_alarm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""Makes the Hue Lamp(s) ping or blink."""
+
+import dbus
+from settings import lamp_addresses
+
+while True:
+    x = input("You can make the lamps (p)ing once or (b)link 15 times and (s)top blinking while blinking : ")
+    if x == 'p':
+        state = 1
+        break
+    elif x == 'b':
+        state = 2
+        break
+    elif x == 's':
+        state = 0
+        break
+    else:
+        print("Please just input (p)ing, (b)link or (s)top.")
+
+systembus = dbus.SystemBus()
+
+destination = ('org.bluez')
+interface = ('org.bluez.GattCharacteristic1')
+object_paths = lamp_addresses
+
+for object_path in object_paths:
+    objectpath = ("/org/bluez/hci0/dev_" + object_path + "/service0023/char0032")
+    object = systembus.get_object(destination, objectpath)
+    alarm_handle = dbus.Interface(object, interface)
+
+    alarm_handle.WriteValue((dbus.Array([dbus.Byte(state)], dbus.Signature('y'))), (dbus.Dictionary([], dbus.Signature('sv'))))
+   
