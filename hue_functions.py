9fd2ce147a54aed577a237dfe9c7e1fc715bf34d
walter5138_charon <piranha@disroot.org>
2020-02-11 23:06:53 +0100

    Added colored print() output.

--- hue_functions.py
+++ hue_functions.py
@@ -5,7 +5,7 @@ def connect_lamp(lamp_address):
      systembus = dbus.SystemBus()
      destination = ('org.bluez')
      interface = ('org.bluez.Device1')
-     objectpath = ("/org/bluez/hci0/dev_" + lamp_address)
+     objectpath = ('/org/bluez/hci0/dev_' + lamp_address)
      object = systembus.get_object(destination, objectpath)
      connection_handle = dbus.Interface(object, interface)
      connection_handle.Connect()
@@ -15,19 +15,20 @@ def get_connection_status(lamp_address):
     systembus = dbus.SystemBus()
     destination = ('org.bluez')
     interface = ('org.freedesktop.DBus.Properties')
-    objectpath = ("/org/bluez/hci0/dev_" + lamp_address)
+    objectpath = ('/org/bluez/hci0/dev_' + lamp_address)
     object = systembus.get_object(destination, objectpath)
     connection_test_handle = dbus.Interface(object, interface)
-    x = connection_test_handle.Get((dbus.String("org.bluez.Device1")), (dbus.String("Connected")))
+    x = connection_test_handle.Get((dbus.String('org.bluez.Device1')), (dbus.String('Connected')))
     return (bool(x))
        
 def connect(lamp_addresses):
+    from termcolor import colored, cprint
     for lamp_address in lamp_addresses:
         status = get_connection_status(lamp_address)
         if status == False:
-            print("Lamp " + lamp_address + " connecting")
+            print('Lamp ' + lamp_address + colored(' connecting ', 'yellow', attrs=['blink']), end='\r')
             connect_lamp(lamp_address)
             status = get_connection_status(lamp_address)
         if status == True:
-            print("Lamp " + lamp_address + " connected")
+            print('Lamp ' + lamp_address + colored(' connected  ' , 'green'))
             
