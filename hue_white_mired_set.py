84d0fad7fc16200ac08b8f5ac097ef5332478a46
walter5138_charon <piranha@disroot.org>
2020-02-13 08:42:59 +0100

    On the way to class.

--- hue_white_mired_set.py
+++ hue_white_mired_set.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
+"""Sets the mired of the Hue Lamp(s)."""
 
+import dbus
+from hue_config import instances
+from hue_class import mired_set, mired_get
+
+for instance in instances:
+    instance.connect()
