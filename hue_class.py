8ad1e19ad71fc429ac3268a3a2a9a7cdb65e0c1f
walter5138_charon <piranha@disroot.org>
2020-03-03 22:02:21 +0100

    Get proxies inline. Saves variables.

--- hue_class.py
+++ hue_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 class HueLamp:
-    """HueLamp class representing and interact with Philips Bluetooth Hue Lamps."""
+    """HueLamp class representing and interact with Philips Hue Bluetooth Lamps."""
 
     def __init__(self, address, name="Hue_Lamp"):
         import time
@@ -10,7 +10,7 @@ class HueLamp:
         self.prop_chg_notify = subprocess.Popen(["./hue_sig_res.py", self.address, name])
         print("subprocess %s pid is %s" % (self.prop_chg_notify.args, self.prop_chg_notify.pid))
         self.connect()
-        time.sleep(4)
+        time.sleep(3)
         self.name_set(name)
         self.name = self.name_get()
         print('%s = %s, connection state: %s' % (self.address, self.name, self.connection_state()))
@@ -18,43 +18,28 @@ class HueLamp:
     def connection_state(self):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ('/org/bluez/hci0/dev_' + self.address)
-        interface = ('org.freedesktop.DBus.Properties')
-        object = systembus.get_object(destination, objectpath)
-        connection_state_handle = dbus.Interface(object, interface)
-        x = connection_state_handle.Get((dbus.String('org.bluez.Device1')), (dbus.String('Connected')))
+        #            ..._handle = dbus.Interface(systembus.get_object('destination', 'objectpath                         '), 'interface                      ')
+        connection_state_handle = dbus.Interface(systembus.get_object('org.bluez'  , '/org/bluez/hci0/dev_' + self.address), 'org.freedesktop.DBus.Properties')
+        x = connection_state_handle.Get('org.bluez.Device1', 'Connected')
         return (bool(x))
 
     def connect(self):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ('/org/bluez/hci0/dev_' + self.address)
-        interface = ('org.bluez.Device1')
-        object = systembus.get_object(destination, objectpath)
-        connect_handle = dbus.Interface(object, interface)
+        connect_handle = dbus.Interface(systembus.get_object('org.bluez', '/org/bluez/hci0/dev_' + self.address), 'org.bluez.Device1')
         connect_handle.Connect()
 
     def name_get(self):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0011/char0014")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        name_get_handle = dbus.Interface(object, interface)
+        name_get_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0011/char0014"), 'org.bluez.GattCharacteristic1')
         x = name_get_handle.ReadValue([])
         return ''.join([str(v) for v in x])
 
     def name_set(self, name):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0011/char0014")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        name_set_handle = dbus.Interface(object, interface)
+        name_set_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0011/char0014"), 'org.bluez.GattCharacteristic1')
         
         ascii_list = [ord(c) for c in name]                 # comment
 
@@ -63,22 +48,14 @@ class HueLamp:
     def on_off_switch(self, switch):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char0026")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        on_off_switch_handle = dbus.Interface(object, interface)
+        on_off_switch_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char0026"), 'org.bluez.GattCharacteristic1')
         on_off_switch_handle.WriteValue([switch], [])
 
     def on_off_state(self):
         import dbus
         from termcolor import colored
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char0026")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        on_off_state_handle = dbus.Interface(object, interface)
+        on_off_state_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char0026"), 'org.bluez.GattCharacteristic1')
         ay = on_off_state_handle.ReadValue([])
 
         if ay[0] == 0:        # ReadValue returns an array of bytes now stored in the variable ay.
@@ -89,11 +66,7 @@ class HueLamp:
     def mired_set(self, mired):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char002c")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        mired_set_handle = dbus.Interface(object, interface)
+        mired_set_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char002c"), 'org.bluez.GattCharacteristic1')
 
         y = mired.to_bytes(2, 'little')                     # comment
 
@@ -102,11 +75,7 @@ class HueLamp:
     def mired_get(self):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char002c")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        mired_get_handle = dbus.Interface(object, interface)
+        mired_get_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char002c"), 'org.bluez.GattCharacteristic1')
         ay = mired_get_handle.ReadValue([])
 
         x = bytes(reversed(ay)).hex()                       # comment
@@ -116,21 +85,13 @@ class HueLamp:
     def alert_set(self, state):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char0032")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        alert_set_handle = dbus.Interface(object, interface)
+        alert_set_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char0032"), 'org.bluez.GattCharacteristic1')
         alert_set_handle.WriteValue([state], [])
                        
     def brightness_get(self):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char0029")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        brightness_get_handle = dbus.Interface(object, interface)
+        brightness_get_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char0029"), 'org.bluez.GattCharacteristic1')
         ay = brightness_get_handle.ReadValue(dbus.Dictionary([], dbus.Signature('sv')))
 
         return int(ay[0])     # ReadValue returns an array of bytes now stored in the variable ay.
@@ -140,21 +101,13 @@ class HueLamp:
     def brightness_set(self, bri):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char0029")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        brightness_set_handle = dbus.Interface(object, interface)
+        brightness_set_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char0029"), 'org.bluez.GattCharacteristic1')
         brightness_set_handle.WriteValue([bri], [])
 
     def transitiontime_get(self):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char0037")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        transitiontime_get_handle = dbus.Interface(object, interface)
+        transitiontime_get_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char0037"), 'org.bluez.GattCharacteristic1')
         ay = transitiontime_get_handle.ReadValue(dbus.Dictionary([], dbus.Signature('sv')))
 
         return float(ay[0] / 10)                # ReadValue returns an array of bytes now stored in the variable ay.
@@ -166,21 +119,13 @@ class HueLamp:
     def transitiontime_set(self, trans):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char0037")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        transitiontime_set_handle = dbus.Interface(object, interface)
+        transitiontime_set_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char0037"), 'org.bluez.GattCharacteristic1')
         transitiontime_set_handle.WriteValue([trans, 0], [])
 
     def color_get(self):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char002f")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        color_get_handle = dbus.Interface(object, interface)
+        color_get_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char002f"), 'org.bluez.GattCharacteristic1')
         col = color_get_handle.ReadValue([])
 
         return [int(z) for z in col]
@@ -188,11 +133,7 @@ class HueLamp:
     def color_set(self, col):
         import dbus
         systembus = dbus.SystemBus()
-        destination = ('org.bluez')
-        objectpath = ("/org/bluez/hci0/dev_" + self.address + "/service0023/char002f")
-        interface = ('org.bluez.GattCharacteristic1')
-        object = systembus.get_object(destination, objectpath)
-        color_set_handle = dbus.Interface(object, interface)
+        color_set_handle = dbus.Interface(systembus.get_object('org.bluez', "/org/bluez/hci0/dev_" + self.address + "/service0023/char002f"), 'org.bluez.GattCharacteristic1')
         color_set_handle.WriteValue(col, [])                   # comment
 
 
